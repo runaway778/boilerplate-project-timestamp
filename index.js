f029ff2b848b3f3e389e2e82ab03c8e7bd8de159
runaway778 <batyrov-amir@bk.ru>
2023-03-23 11:12:33 +0300

    Set default port

--- index.js
+++ index.js
@@ -42,6 +42,6 @@ app.get("/api/:date?", (req, res) => {
 });
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
